eaf06e4b6b947b336f6845d77674b28c457e5e3e
forumulator <forumulator@gmail.com>
2016-06-01 12:06:23 +0530

    Began a crude assembler for Leros

---
+++ asm/codes.py
@@ -0,0 +1,47 @@
+opttab={'NOP':0x00}
+
+opttab['DB']=[2,]
+opttab['DA']=[2,]
+
+opttab['ADD'] = 0x08
+opttab['ADDI'] = 0x09
+opttab['SUB'] = 0x0c
+opttab['SUBI'] = 0x08
+
+opttab['SHR'] = 0x10
+opttab['LOAD'] = 0x20
+opttab['LDI'] = 0x21
+
+opttab['AND'] = 0x22
+opttab['ANDI'] = 0x23
+
+opttab['OR'] = 0x24
+opttab['ORI'] = 0x25
+opttab['XOR'] = 0x26
+opttab['XORI'] = 0x27
+
+opttab['LOADH'] = 0x28
+opttab['LDHI'] = 0x29
+
+opttab['STORE'] = 0x30
+
+opttab['OUT'] = 0x38
+opttab['IN'] = 0x3c
+
+opttab['JAL'] = 0x40
+
+opttab['LOADADDR'] = 0x50
+
+# Load accumulator indirect
+opttab['LDAX'] = 0x60
+opttab['STAX'] = 0x70
+
+
+def to_bin(x,num):
+	x=int(x)
+	ans=str('')
+	for i in range(num):
+		ans=str(x%2)+ans
+		x=int(x/2)
+
+	return ans	
