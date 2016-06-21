fe0439dcfe818557696dca66859a5d5da617631d
forumulator <forumulator@gmail.com>
2016-06-22 03:02:06 +0530

    Added .travis.yml and build scripts

---
+++ scripts/ci/install_myhdl.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git clone https://github.com/jandecaluwe/myhdl
+cd myhdl
+python setup.py install
---
+++ scripts/ci/install_rhea.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git clone https://github.com/cfelton
+cd rhea
+python setup.py install
