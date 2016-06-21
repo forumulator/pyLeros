9a5b81b25065ec4a0be5da16ab5eb9e8780760d6
forumulator <forumulator@gmail.com>
2016-06-22 03:32:17 +0530

    Modified tests to use the new block object

--- test/test_decoder.py
+++ test/test_decoder.py
@@ -4,17 +4,14 @@ from pyleros.types import alu_op_type, t_decSignal
 from pyleros import decoder
 from myhdl import *
 
-from rhea.system import Clock, Reset
 
-from rhea.utils.test import run_testbench
-
-
-def test_decode(args=None):
+@block
+def tb_dec_top(args=None):
 	"""Test the decoder module in pyleros
 
 	"""
 	clock = Signal(bool(0))
-	reset = Reset(0, active=0, async=True)
+	reset = ResetSignal(0, active=0, async=True)
 
 	# the high 8 bits of the instruction
 	instr_hi = Signal(intbv(0)[8:])
@@ -32,90 +29,91 @@ def test_decode(args=None):
 		clock.next = not clock
 
 
-	def _bench_dec():
 
+	# instantiate the decoder
+	decode_inst = decoder.pyleros_decoder(instr_hi, out_list)
 
-		# instantiate the decoder
-		decode_inst = decoder.pyleros_decoder(instr_hi, out_list)
+	@instance
+	def tbstim():
 
-		@instance
-		def tbstim():
+		for i in range(5):
+			yield clock.posedge
 
-			for i in range(5):
-				yield clock.posedge
+		for instr in codes:
 
-			for instr in codes:
+			# set the input to the decoder
+			instr_op = codes[instr][0]
+			instr_hi.next = instr_op
+			yield delay(33)
 
-				# set the input to the decoder
-				instr_op = codes[instr][0]
-				instr_hi.next = instr_op
+			# check for correct decode
+			for cs in dlist:
+				if not cs == t_decSignal.op:
+					if cs in codes[instr][1]:
+						try:
+							assert d[str(cs)] == True
+						except Exception as e:
+							print(instr)
+							print(cs)
+							raise e
+
+					else:
+						try:
+							assert d[str(cs)] == False
+						except Exception as e:
+							print(instr)
+							print(cs)
+							raise e
+
+				else:
+					if cs in codes[instr][1]:
+						assert d[str(cs)] == codes[instr][3]
+
+			yield delay(33)
+
+			if codes[instr][2] == True:
+
+				instr_imm = instr_op | 0x01
+				instr_hi.next = instr_imm
 				yield delay(33)
 
 				# check for correct decode
 				for cs in dlist:
-					if not cs == t_decSignal.op:
+					if not ((cs == t_decSignal.op) or (cs == t_decSignal.sel_imm)):
 						if cs in codes[instr][1]:
 							try:
 								assert d[str(cs)] == True
 							except Exception as e:
 								print(instr)
 								print(cs)
-								raise e
-
+								raise e								
 						else:
-							try:
-								assert d[str(cs)] == False
-							except Exception as e:
-								print(instr)
-								print(cs)
-								raise e
+							
+							assert d[str(cs)] == False
 
 					else:
 						if cs in codes[instr][1]:
 							assert d[str(cs)] == codes[instr][3]
 
-				yield delay(33)
+					assert d['sel_imm'] == True
 
-				if codes[instr][2] == True:
-
-					instr_imm = instr_op | 0x01
-					instr_hi.next = instr_imm
-					yield delay(33)
-
-					# check for correct decode
-					for cs in dlist:
-						if not ((cs == t_decSignal.op) or (cs == t_decSignal.sel_imm)):
-							if cs in codes[instr][1]:
-								try:
-									assert d[str(cs)] == True
-								except Exception as e:
-									print(instr)
-									print(cs)
-									raise e								
-							else:
-								
-								assert d[str(cs)] == False
-
-						else:
-							if cs in codes[instr][1]:
-								assert d[str(cs)] == codes[instr][3]
-
-						assert d['sel_imm'] == True
-
-				for ii in range(5):
-					yield clock.posedge
+			for ii in range(5):
+				yield clock.posedge
 
 
-			raise StopSimulation
+		raise StopSimulation
 
-		return tbstim, decode_inst, tbclk
+	return instances()
 
-	run_testbench(_bench_dec)
 
+def test_decoder():
+
+	inst_top = tb_dec_top()
+	inst_top.run_sim()
 
 if __name__ == "__main__":
 
-	test_rom()
+	test_decoder()
 
 
 
--- test/test_rom.py
+++ test/test_rom.py
@@ -3,10 +3,6 @@ from pyleros.types import IM_BITS
 
 from myhdl import *
 
-from rhea.system import Clock, Reset
-
-from rhea.utils.test import run_testbench
-
 import random
 from random import randrange
 from datetime import datetime
@@ -14,12 +10,13 @@ from datetime import datetime
 random.seed(int(datetime.now().time().second))
 
 
-def test_rom(args=None):
+@block
+def top_block(args=None):
 	"""Test the rom module in pyleros
 
 	"""
 	clock = Signal(bool(0))
-	reset = Reset(0, active=1, async=True)
+	reset = ResetSignal(0, active=1, async=True)
 
 	rd_addr = Signal(intbv(0)[IM_BITS:])
 	rd_data = Signal(intbv(0)[16:]) 
@@ -33,58 +30,57 @@ def test_rom(args=None):
 		clock.next = not clock
 
 	
+	# instantiate the rom
+	for i in range(IM_SIZE):
+		instr_array[i] = randrange(2**15) - 2**14
+		# if i < 10:
+		# 	print(instr_array[i])
+	
+	im_inst = rom.pyleros_im(clock, reset, rd_addr, rd_data, filename = instr_array)
+	
 
-	def _bench_dec():
+	@instance
+	def tbstim():
 
-		# instantiate the rom
-		for i in range(IM_SIZE):
-			instr_array[i] = randrange(2**15) - 2**14
-			# if i < 10:
+		for i in range(2):
+			yield clock.posedge
+
+		for tryn in range(IM_SIZE):
+			
+			addr = randrange(IM_SIZE)
+
+			rd_addr.next = addr
+
+			for i in range(1):
+				yield clock.posedge
+
+			# this delay is necessary because when on positive
+			# edge, the rd_data.next attribute is changed ny the
+			# IM, the actual rd_data changes in the next simulation step.
+			yield delay(1)
+
+			# for i in range(10):
 			# 	print(instr_array[i])
-		
-		im_inst = rom.pyleros_im(clock, reset, rd_addr, rd_data, filename = instr_array)
-		
-
-		@instance
-		def tbstim():
-
-			for i in range(2):
-				yield clock.posedge
-
-			for tryn in range(IM_SIZE):
-				
-				addr = randrange(IM_SIZE)
-
-				rd_addr.next = addr
-
-				for i in range(1):
-					yield clock.posedge
-
-				# this delay is necessary because when on positive
-				# edge, the rd_data.next attribute is changed ny the
-				# IM, the actual rd_data changes in the next simulation step.
-				yield delay(1)
-
-				# for i in range(10):
-				# 	print(instr_array[i])
-				# print("\n")
-				# print(addr)
-				assert (rd_data.next & 0xffff) == (instr_array[addr] & 0xffff)
+			# print("\n")
+			# print(addr)
+			assert (rd_data.next & 0xffff) == (instr_array[addr] & 0xffff)
 
 
 
-			for ii in range(5):
-				yield clock.posedge
+		for ii in range(5):
+			yield clock.posedge
 
 
-			raise StopSimulation
+		raise StopSimulation
 
-		return tbclk, tbstim, im_inst
-
-	for jj in range(10):
-		run_testbench(_bench_dec)
+	return instances()
 
 
+def test_rom():
+
+	for ii in range(10):
+		inst_block = top_block()
+		inst_block.run_sim()
 
 if __name__ == "__main__":
 
